8185619d4c956be6b7b438d29cce2c283574bf08
smgrebe <smgrebe@ncsu.edu>
2023-02-11 14:15:12 -0500

    Initial commit.

---
+++ app/src/main/java/com/example/pearlhacks23/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.pearlhacks23
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pearlhacks23.ui.theme.PearlHacks23Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PearlHacks23Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PearlHacks23Theme {
+        Greeting("Android")
+    }
+}
